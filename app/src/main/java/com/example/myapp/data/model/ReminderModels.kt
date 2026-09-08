27df528220032d915952c9f01f378ae7efe0d79f
stylishtyper <stylishtyper@gmail.com>
2026-09-08 16:50:48 +0600

    Create ReminderModels.kt

---
+++ app/src/main/java/com/example/myapp/data/model/ReminderModels.kt
@@ -0,0 +1,24 @@
+package com.akhirah.reminder.data.model
+
+data class QuranAyah(
+    val arabic: String,
+    val bengali: String,
+    val reference: String,
+    val audioUrl: String
+)
+
+data class HadithContent(
+    val arabic: String,
+    val bengali: String,
+    val reference: String
+)
+
+data class ReminderSessionData(
+    val id: Int,
+    val ayah: QuranAyah,
+    val hadith: HadithContent,
+    val fallbackNews: String,
+    val hasTasbih: Boolean = true,
+    val hasDonation: Boolean = false,
+    val donationUrl: String = "https://assunnahfoundation.org"
+)
